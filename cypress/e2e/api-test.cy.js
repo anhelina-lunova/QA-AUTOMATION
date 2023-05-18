a9559317558e223f5c5ebd5687e3031604b5653f
Anhelina Lunova <120516170+anhelina-lunova@users.noreply.github.com>
2023-05-18 13:06:17 +0300

    Update api-test.cy.js

--- cypress/e2e/api-test.cy.js
+++ cypress/e2e/api-test.cy.js
@@ -49,22 +49,113 @@
 // });
 
 //
-const request = {
-  method: "GET",
-  url: "https://httpbin.org/headers",
-  headers: {
-    customHeader: "customValue",
-  },
-  failOnStatusCode: false,
-};
 
-describe("httpbin tests", () => {
-  it("responce code should be 200", () => {
-    cy.request(request).then((response) => {
-      assert.equal(200, response.status);
+// const request = {
+//   method: "GET",
+//   url: "https://httpbin.org/headers",
+//   headers: {
+//     customHeader: "customValue",
+//   },
+//   failOnStatusCode: false,
+// };
 
-      //порівнює типи даних: обʼєкт та JSON
-      assert.equal("customValue", response.requestHeaders.customHeader);
-    });
+// describe("httpbin tests", () => {
+//   it("responce code should be 200", () => {
+//     cy.request(request).then((response) => {
+//       assert.equal(200, response.status);
+
+//       assert.equal("customValue", response.requestHeaders.customHeader);
+//     });
+//   });
+// });
+
+//
+
+// const request = {
+//   method: "GET",
+//   url: "https://httpbin.org/headers",
+//   headers: {
+//     "user-agent": "My test user-agent",
+//   },
+//   failOnStatusCode: false,
+// };
+
+// describe("httpbin tests", () => {
+//   it("responce code should be 200", () => {
+//     cy.request(request).then((response) => {
+//       assert.equal(200, response.status);
+
+//       assert.equal("My test user-agent", response.requestHeaders["user-agent"]);
+//     });
+//   });
+// });
+
+//
+
+// const request = {
+//   method: "GET",
+//   url: "https://httpbin.org/headers",
+//   headers: {
+//     Cookie: "cookieName=cookieValue",
+//   },
+//   failOnStatusCode: false,
+// };
+
+// describe("httpbin tests", () => {
+//   it("responce code should be 200", () => {
+//     cy.request(request).then((response) => {
+//       assert.equal(200, response.status);
+
+//       assert.equal("cookieName=cookieValue", response.requestHeaders.Cookie);
+//     });
+//   });
+// });
+
+//
+
+// const request = {
+//   method: "POST",
+//   url: "https://reqres.in/api/users",
+//   body: {
+//     name: "Angie",
+//     job: "AQAE",
+//   },
+// };
+
+// describe("httpbin tests", () => {
+//   it("responce code should be 201", () => {
+//     cy.request(request).then((response) => {
+//       //debugger;
+
+//       assert.equal(201, response.status);
+
+//       assert.equal("Angie", response.body.name);
+//       cy.log(response);
+
+//       assert.isTrue(response.duration <= 1000);
+//     });
+//   });
+// });
+
+//
+
+describe("test with faker", () => {
+  it("???", () => {
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * max);
+    }
+
+    for (let index = 0; index < 10; index++) {
+      const randomID = getRandomInt(999);
+
+      const request = {
+        url: "https://httpbin.org/headers",
+        id: randomID,
+      };
+
+      cy.request(request).then((response) => {
+        assert.isTrue(response.status == 200);
+      });
+    }
   });
 });
